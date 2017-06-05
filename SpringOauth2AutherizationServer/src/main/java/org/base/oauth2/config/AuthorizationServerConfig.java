8fb7b326c44f61f2427cbbc56e29990975a67120
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2017-06-05 15:19:08 +0530

    working jwt

--- SpringOauth2AutherizationServer/src/main/java/org/base/oauth2/config/AuthorizationServerConfig.java
+++ SpringOauth2AutherizationServer/src/main/java/org/base/oauth2/config/AuthorizationServerConfig.java
@@ -1,16 +1,20 @@
 package org.base.oauth2.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 /**
  * <p>
@@ -34,25 +38,42 @@ import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
 	@Autowired
-	private DataSource dataSource;
-
-	@Bean
-	public TokenStore tokenStore() {
-		return new JdbcTokenStore(dataSource);
-	}
+	private Environment environment;
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.tokenStore(tokenStore());
+		endpoints.tokenStore(tokenStore()).tokenEnhancer(jwtTokenEnhancer())
+				.authenticationManager(authenticationManager);
+	}
+
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(jwtTokenEnhancer());
+	}
+
+	@Bean
+	protected JwtAccessTokenConverter jwtTokenEnhancer() {
+		String pwd = environment.getProperty("keystore.password");
+		KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("jwt.jks"),
+				pwd.toCharArray());
+		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		converter.setKeyPair(keyStoreKeyFactory.getKeyPair("jwt"));
+		return converter;
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("curl").authorities("ROLE_ADMIN").resourceIds("jaxenter").scopes("read", "write")
-				.authorizedGrantTypes("client_credentials").secret("password").and().withClient("web")
-				.redirectUris("http://github.com/techdev-solutions/").resourceIds("jaxenter").scopes("read")
-				.authorizedGrantTypes("implicit");
+		clients.inMemory().withClient("service-account-1").secret("service-account-1-secret").authorities("ROLE_ADMIN")
+				.authorizedGrantTypes("client_credentials").scopes("resource-server-read", "resource-server-write")
+				.accessTokenValiditySeconds(300);
 	}
 }
--- SpringOauth2ResourceServer/src/main/java/org/base/oauth2/SpringOauth2ResourceServer.java
+++ SpringOauth2ResourceServer/src/main/java/org/base/oauth2/SpringOauth2ResourceServer.java
@@ -1,12 +1,17 @@
 package org.base.oauth2;
 
 import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -28,18 +33,33 @@ import org.springframework.web.bind.annotation.RestController;
  * 
  * </pre>
  */
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 @RestController
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableResourceServer
 public class SpringOauth2ResourceServer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringOauth2ResourceServer.class, args);
 	}
 
-	@RequestMapping("/user")
-	public Principal user(Principal user) {
-		return user;
+	private String message = "Hello world!";
+
+	@PreAuthorize("#oauth2.hasScope('resource-server-read')")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public Map<String, String> home() {
+		return Collections.singletonMap("message", message);
+	}
+
+	@PreAuthorize("#oauth2.hasScope('resource-server-write')")
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public void updateMessage(@RequestBody String message) {
+		this.message = message;
+	}
+
+	@PreAuthorize("#oauth2.hasScope('resource-server-read')")
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public Map<String, String> user(Principal user) {
+		return Collections.singletonMap("message", "user is: " + user.toString());
 	}
 }
